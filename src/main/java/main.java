4b82adee0b6461e2f32f0c7cc3054883d62a4774
Miller Paiva <dev.millerp@gmail.com>
2024-11-20 00:27:14 -0500

    Se agregó funcionalidad

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,5 @@
 public class main {
-//    public static void main(String[] args){
-//        System.out.println("Hola mundo");
-//    }
-//}
-
     public static void main(String[] args) {
-        try {
-            int result = 10 / 0; // Genera una excepción
-        } catch (ArithmeticException e) {
-            System.out.println("No se puede dividir por cero!");
-        }
+        System.out.println("Welcome to JavaBank ATM! Version control with Git.");
     }
 }
